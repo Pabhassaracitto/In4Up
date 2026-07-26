0483df732ea981f87ad3390dc1f433cfbddd163a
Pabhassaracitto <pabhassaracitto@gmail.com>
2026-07-26 23:58:27 +0530

    feat: Update NDK configuration to dynamically set version based on OS

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,28 @@
 import com.android.build.gradle.BaseExtension
+import org.apache.tools.ant.taskdefs.condition.Os
+import java.util.Properties
 
+// 1. TỰ ĐỘNG CẬP NHẬT local.properties THEO OS
+val localPropsFile = file("local.properties")
+val localProps = Properties()
+
+if (localPropsFile.exists()) {
+    localPropsFile.inputStream().use { localProps.load(it) }
+}
+
+val isWindows = Os.isFamily(Os.FAMILY_WINDOWS)
+
+if (isWindows) {
+    localProps.remove("ndk.dir")
+} else {
+    val linuxSdk = localProps.getProperty("sdk.dir") ?: "/media/pabahassara/MEDIA/LINUX/LIBRARY/Android/Sdk"
+    localProps.setProperty("ndk.dir", "$linuxSdk/ndk/27.3.13750724")
+}
+
+localPropsFile.outputStream().use { localProps.store(it, "Auto-generated NDK path by Gradle") }
+
+
+// 2. CẤU HÌNH DỰ ÁN VÀ ÉP NDK CHO TẤT CẢ SUBPROJECTS BEFORE EVALUATE
 allprojects {
     repositories {
         google()
@@ -9,19 +32,22 @@ allprojects {
 
 rootProject.layout.buildDirectory.set(file("../build"))
 
+val targetNdkVersion = if (isWindows) "28.2.13676358" else "27.3.13750724"
+
 subprojects {
     project.layout.buildDirectory.set(file("${rootProject.layout.buildDirectory.get()}/${project.name}"))
-    project.evaluationDependsOn(":app")
 
-    plugins.withId("com.android.application") {
-        extensions.configure<BaseExtension> {
-            ndkVersion = "27.0.12077973"
+    // CHÌA KHÓA Ở ĐÂY: Can thiệp TRƯỚC KHI plugin phụ thuộc (như :jni) kịp đọc config cũ
+    beforeEvaluate {
+        plugins.withId("com.android.application") {
+            extensions.configure<BaseExtension> {
+                ndkVersion = targetNdkVersion
+            }
         }
-    }
-
-    plugins.withId("com.android.library") {
-        extensions.configure<BaseExtension> {
-            ndkVersion = "27.0.12077973"
+        plugins.withId("com.android.library") {
+            extensions.configure<BaseExtension> {
+                ndkVersion = targetNdkVersion
+            }
         }
     }
 }
